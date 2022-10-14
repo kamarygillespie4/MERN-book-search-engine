639ab131ff244e4b4e874026776f2915642b68a1
Kamary <gillespiekamary@gmail.com>
2022-10-13 19:32:27 -0600

    created client query for get me

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,5 @@
 import { gql } from "@apollo/client";
 
-import gql from "graphql-tag";
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
